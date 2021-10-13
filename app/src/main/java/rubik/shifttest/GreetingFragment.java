5c5dd34ee7f14e9252fb19f16bd90c02ab48ce43
CandyRubik <rubikstudio2002@gmail.com>
2021-10-13 12:18:23 +0700

    created is a simple application with two fragments communicating with each other

--- app/src/main/java/rubik/shifttest/GreetingFragment.java
+++ app/src/main/java/rubik/shifttest/GreetingFragment.java
@@ -2,18 +2,37 @@ package rubik.shifttest;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class GreetingFragment extends Fragment {
 
+    private String message;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_greeting, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Bundle args = getArguments();
+
+        if(args != null) {
+            message = args.getString("Message");
+        } else {
+            message = "No message was transmitted";
+        }
+        TextView greeting = view.findViewById(R.id.greeting_text);
+        greeting.setText(message);
     }
 }
