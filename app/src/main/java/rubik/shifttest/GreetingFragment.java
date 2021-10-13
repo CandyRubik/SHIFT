27e61da61345083540c13ec50626f7d33c29ad48
CandyRubik <rubikstudio2002@gmail.com>
2021-10-13 11:38:01 +0700

    initial two fragment app

---
+++ app/src/main/java/rubik/shifttest/GreetingFragment.java
@@ -0,0 +1,19 @@
+package rubik.shifttest;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class GreetingFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_greeting, container, false);
+    }
+}
---
+++ app/src/main/java/rubik/shifttest/SignUpFragment.java
@@ -0,0 +1,21 @@
+package rubik.shifttest;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SignUpFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+    }
+
+
+}
